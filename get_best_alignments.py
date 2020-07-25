7f75f0d397f3c9a1135857a8909edf79dc6910fb
Saviour Owolabi <owolabisaviour@gmail.com>
2020-07-25 14:50:31 +0000

    added english vocabulary file, modified get_best_alignments.py to show moses translations in dictionary output

--- get_best_alignments.py
+++ get_best_alignments.py
@@ -1,25 +1,30 @@
 import pprint
-pidgin_words = set()
+pidgin_words = list()
+moses_translations = list()
 
 alignment_dict = {}
 
-with open('unique.vcb.pcm', 'r') as f: 
+with open('pre_translation_candidates', 'r') as f: 
     for line in f: 
-        pidgin_words.add(line.strip())
+        pidgin_words.append(line.strip())
 
-with open('100mostcommon.pcm', 'r') as f: 
+with open('moses_translations', 'r') as f: 
     for line in f: 
-        pidgin_words.add(line.strip())
+        moses_translations.append(line.strip())
+        
 
 with open('lex.f2e', 'r') as f: 
     for line in f: 
         values = line.strip().split(' ')
-        if values[1] in pidgin_words and values[1] not in alignment_dict.keys():
-            alignment_dict[values[1]] = {'words': [], 'probs': []}
-        
-        if values[1] in pidgin_words and values[1] in alignment_dict.keys():
-            alignment_dict[values[1]]['words'].append(values[1])
-            alignment_dict[values[1]]['probs'].append(values[2])
+        if values[0] != "NULL":
+            if values[1] in pidgin_words and values[1] not in alignment_dict.keys() :
+                alignment_dict[values[1]] = {'moses_trans':'', 'words': [], 'probs': []}
+                idx = pidgin_words.index(values[1])
+                alignment_dict[values[1]]['moses_trans'] = moses_translations[idx]
+            
+            if values[1] in pidgin_words and values[1] in alignment_dict.keys():
+                alignment_dict[values[1]]['words'].append(values[0])
+                alignment_dict[values[1]]['probs'].append(values[2])
 
 
 for keyvalue in alignment_dict.keys(): 
